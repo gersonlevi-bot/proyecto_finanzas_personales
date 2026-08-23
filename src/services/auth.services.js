4c17ec02e62dc3abc67ca4d78c8016f8d02ddbfd
gersonLevi <gersonlevigonzalesurbina@gmail.com>
2026-08-23 00:03:25 -0500

    fix: corregir errores de sintaxis

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -2,7 +2,7 @@ import { hash } from "bcrypt";
 import { searchByEmail, saveUser } from "../repositories/user.repository.js";
 
 export async function registerUser(datos) {
-    const SALT_ROUNDS = 12
+    const SALT_ROUNDS = 12;
     const emailRegex =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const pwdRegex = /^(?=.*[a-z])(?=(.*[A-Z]))(?=.*\d)(?=.*[\W_]).{8,32}$/;
 
@@ -18,30 +18,35 @@ export async function registerUser(datos) {
 
     if (!pwdRegex.test(password)) {
         throw new Error(
-        "La contraseña no cumple con los requisitos de seguridad");
+            "La contraseña no cumple con los requisitos de seguridad");
     }
 
     const foundUser = await searchByEmail(email);
-    if (!foundUser) {
-        throw new Error("l correo electrónico ya está registrado");
+    if (foundUser) {
+        throw new Error("El correo electrónico ya está registrado");
     }
 
-    const passwordHasheada = hash(password, SALT_ROUNDS)
+    const passwordHasheada = await hash(password, SALT_ROUNDS);
+    let id;
 
     try {
-        const id = await saveUser({name, last_name, email, passwordHasheada})
+        id = await saveUser({
+            name,
+            last_name,
+            email,
+            password_hash: passwordHasheada,
+        });
     } catch (error) {
-        if(error.code === 'ER_DUP_ENTRY' || error.errno === 1062) {
-            throw new Error('EMAIL_DUPLICADO_CONCURRENTE');
+        if (error.code === "ER_DUP_ENTRY" || error.errno === 1062) {
+            throw new Error("EMAIL_DUPLICADO_CONCURRENTE");
         }
 
         console.error("Error en la inserción física:", error);
         throw error;
     }
 
-    return { 
+    return {
         message: "Usuario registrado correctamente",
-        id
-    }
-
+        id: id,
+    };
 }
