5044539e894b597aabf3bfd003964cf18a28761c
gersonLevi <gersonlevigonzalesurbina@gmail.com>
2026-08-23 12:03:44 -0500

    fix

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -3,27 +3,34 @@ import { searchByEmail, saveUser } from "../repositories/user.repository.js";
 
 export async function registerUser(datos) {
     const SALT_ROUNDS = 12;
-    const emailRegex =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const emailRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const pwdRegex = /^(?=.*[a-z])(?=(.*[A-Z]))(?=.*\d)(?=.*[\W_]).{8,32}$/;
 
     const { name, last_name, email, password } = datos;
 
     if (!name || !last_name || !email || !password) {
-        throw new Error("Todos los campos son obligatorios");
+        const error = new Error("Todos los campos son obligatorios");
+        error.statusCode = 400;
+        throw error;
     }
 
     if (!emailRegex.test(email)) {
-        throw new Error("El formato del correo no es válido");
+        const error = new Error("El formato del correo no es válido");
+        error.statusCode = 400;
+        throw error;
     }
 
     if (!pwdRegex.test(password)) {
-        throw new Error(
-            "La contraseña no cumple con los requisitos de seguridad");
+        const error = new Error("La contraseña no cumple con los requisitos de seguridad",);
+        error.statusCode = 400;
     }
 
     const foundUser = await searchByEmail(email);
     if (foundUser) {
-        throw new Error("El correo electrónico ya está registrado");
+        const error = new Error("El correo electrónico ya está registrado");
+        error.statusCode = 400;
+        throw error;
     }
 
     const passwordHasheada = await hash(password, SALT_ROUNDS);
@@ -38,7 +45,9 @@ export async function registerUser(datos) {
         });
     } catch (error) {
         if (error.code === "ER_DUP_ENTRY" || error.errno === 1062) {
-            throw new Error("EMAIL_DUPLICADO_CONCURRENTE");
+            const errorConflict = new Error("El correo electrónico ya fue registrado por otro proceso");
+            errorConflict.statusCode = 409;
+            throw errorConflict;
         }
 
         console.error("Error en la inserción física:", error);
