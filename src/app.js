d560caf23895895c7ef4a01f9835bcf3b78bab51
gersonLevi <gersonlevigonzalesurbina@gmail.com>
2026-08-28 11:04:31 -0500

    fix: arreglar nombre del errorHandler en middleware y añadir la verificacion de token para los endpoint futuros

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import router from "../src/routes/index.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
---
+++ src/middlewares/authToken.middleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import { ErrorApp } from "../utils/ErrorApp";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+export function authToken(req, res, next) {
+    const token = req.headers.authorization?.slice(" ")[1];
+    if (!token) throw new ErrorApp("Acceso denegado. No proporciono el token", 401);
+
+    try {
+        const secret = process.env.JWT_SECRET;
+        const user = jwt.verify(token, secret);
+        req.user = user;
+
+        next();
+    } catch (error) {
+        throw new ErrorApp("Token invalido o expirado", 401);
+    }
+}
