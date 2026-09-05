015a07435ac8c1c95ce15bd646af507292e34757
gersonLevi <gersonlevigonzalesurbina@gmail.com>
2026-09-05 14:31:32 -0500

    fix: corregir error de mensage en la validación de tipo de transacción

--- src/utils/transactionValidators.js
+++ src/utils/transactionValidators.js
@@ -3,5 +3,5 @@ import { ErrorApp } from "./ErrorApp.js";
 export function validateTypeTransaction(type_transaction) {
     const type = ["income", "expense"];
     if (!type.includes(type_transaction))
-        throw new ErrorApp("Opcion no valida en tipo de categoria", 400);
+        throw new ErrorApp("Opcion no valida en tipo de transacción", 400);
 };
