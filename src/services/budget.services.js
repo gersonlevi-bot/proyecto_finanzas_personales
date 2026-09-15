181a47c03ce14055f3950fad3d26de96ffcec311
gersonLevi <gersonlevigonzalesurbina@gmail.com>
2026-09-15 10:02:24 -0500

    fix: añadir validación para las fechas

--- src/services/budget.services.js
+++ src/services/budget.services.js
@@ -11,10 +11,35 @@ export async function createBudgetServices(dataBudget, userId) {
 
     if(typeof amount !== "number" || amount <= 0)
         throw new ErrorApp("El monto debe ser de tipo número y mayor a 0", 400);
+    
+    // valida que las fechas vengas en formato ISO 8601
+    const formatoISO = /^\d{4}-\d{2}-\d{2}$/;
+    if(!formatoISO.test(time_start) || !formatoISO.test(time_end))
+        throw new ErrorApp("Las fechas deben estar estrictamente en formato YYYY-MM-DD", 400);
 
-    if(time_start >= time_end)
+    // convierte las fechas con hora
+    const startDate = new Date(`${time_start}T00:00:00`);
+    const endDate = new Date(`${time_end}T00:00:00`);
+
+    // valida qe la fecha no sea corrupta con letras 
+    if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) 
+        throw new ErrorApp("Una de las fechas provistas no es válida.", 400);
+
+    // desestructura las fechas y convirte en numeros
+    const [ startYear, startMonth, startDay ] = time_start.split("-").map(Number)
+    const [ endYear, endMonth, endDay ] = time_end.split("-").map(Number)
+
+    // valida que las fechas sean correctas y validas en el calendario
+    if(endDate.getFullYear() !== endYear || (endDate.getMonth() + 1) !== endMonth || endDate.getDate() !== endDay)
+        throw new ErrorApp("La fecha de fin no es un día válido en el calendario", 400);
+    
+    if(startDate.getFullYear() !== startYear || (startDate.getMonth() + 1) !== startMonth || startDate.getDate() !== startDay)
+        throw new ErrorApp("La fecha de inicio no es un día válido en el calendario", 400);
+    
+    // valida que la fecha inicio sea menor a la del fin
+    if (startDate.getTime() >= endDate.getTime()) 
         throw new ErrorApp("La fecha de inicio debe ser menor a la del fin", 400);
-        
+    
     const category = await getCategoryByIdServices(category_id, userId);
 
     const idBudget = await db.transaction( async (trx) => {
